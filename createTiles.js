35256919c84cfd39d5df71d83b2a34e8e3a96021
Script Raccoon <javascript.martin@gmail.com>
2020-12-07 11:21:19 +0100

    media queries again

--- createTiles.js
+++ createTiles.js
@@ -1,7 +1,7 @@
 import { COORDINATES } from "./coordinates.js";
 import { TILE_WIDTH, TILE_HEIGHT, images } from "./images.js";
 
-const TILE_OFFSET = 7;
+const TILE_DEPTH = 7;
 const TOTAL_OFFSET_TOP = 30;
 const TOTAL_OFFSET_LEFT = 80;
 const TILE_ROUNDNESS = 7;
@@ -15,8 +15,8 @@ export function createTiles(options) {
         const tile = $("<div></div>")
             .addClass("tile")
             .css({
-                left: x * TILE_WIDTH + TILE_OFFSET * z + TOTAL_OFFSET_LEFT + "px",
-                top: y * TILE_HEIGHT + TILE_OFFSET * z + TOTAL_OFFSET_TOP + "px",
+                left: x * TILE_WIDTH + TILE_DEPTH * z + TOTAL_OFFSET_LEFT + "px",
+                top: y * TILE_HEIGHT + TILE_DEPTH * z + TOTAL_OFFSET_TOP + "px",
                 zIndex: z,
             })
             .attr("coord", coord.toString())
@@ -38,15 +38,15 @@ export function createTiles(options) {
         const tileBack = $("<div></div>")
             .addClass("tileBack")
             .css({
-                width: TILE_WIDTH + TILE_OFFSET + "px",
-                height: TILE_HEIGHT + TILE_OFFSET + "px",
-                top: -TILE_OFFSET + "px",
-                left: -TILE_OFFSET + "px",
+                width: TILE_WIDTH + TILE_DEPTH + "px",
+                height: TILE_HEIGHT + TILE_DEPTH + "px",
+                top: -TILE_DEPTH + "px",
+                left: -TILE_DEPTH + "px",
                 borderRadius: `
                     ${TILE_ROUNDNESS}px
-                    ${2 * TILE_OFFSET}px
+                    ${2 * TILE_DEPTH}px
                     ${TILE_ROUNDNESS}px
-                    ${2 * TILE_OFFSET}px`,
+                    ${2 * TILE_DEPTH}px`,
             });
 
         tile.append(tileBack).append(tileFront).appendTo("#game");
